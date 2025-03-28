8f91eef735875c14f02c60f675a8696985a2f0ca
14kheaven <feelsgood00@yandex.ru>
2025-03-28 09:56:21 +0300

    void commit

---
+++ Gordeev_LR3_project.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <c.math>
+#include <string>
+using namespace std;
+
+double EnterA() {
+
+}
+
+double EnterB() {
+
+}
+
+double FindS() {
+
+}
+
+double FindP() {
+
+}
