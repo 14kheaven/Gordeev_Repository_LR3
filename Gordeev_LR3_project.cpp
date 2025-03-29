81ee1f586cb537142c1d992fb136694e8359c512
14kheaven <feelsgood00@yandex.ru>
2025-03-29 16:49:52 +0300

    final commit

--- Gordeev_LR3_project.cpp
+++ Gordeev_LR3_project.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <c.math>
+#include <cmath>
 #include <string>
 using namespace std;
 
@@ -13,7 +13,9 @@ if (a <= 0) {
     return EnterA();
 }
 cout << "Введеное число A: " << a << endl;
+return a;
 }
+
 
 double EnterB() {
 int b;
@@ -25,16 +27,97 @@ if (b <= 0) {
     return EnterB();
 }
 cout << "Введеное число B: " << b << endl;
+return b;
 }
 
 double FindS() {
 double S;
-s = (a * b);
+ if (a <= 0 || b <= 0) {
+        cout << "Ошибка: сначала введите A и B!" << endl;
+        return 0;
+S = (a * b);
 cout << "Площадь прямоугольника равна: " << S << endl;
+return S;
 }
 
 double FindP() {
 double P;
+ if (a <= 0 || b <= 0) {
+        cout << "Ошибка: сначала введите A и B!" << endl;
+        return 0;
 P = 2*(a + b);
 cout << "Площадь прямоугольника равна: " << P << endl;
+return P;
+}
+
+int main {
+int choice;
+
+    bool exitProgram = false;
+
+
+
+    while (!exitProgram) {
+
+        cout << "\n=== МЕНЮ ===" << endl;
+
+        cout << "1. Ввести число A" << endl;
+
+        cout << "2. Ввести число B" << endl;
+
+        cout << "3. Найти площадь (A × B)" << endl;
+
+        cout << "4. Найти периметр (2 × (A + B))" << endl;
+
+        cout << "5. Выход" << endl;
+
+        cout << "Выберите действие: ";
+
+        cin >> choice;
+
+
+
+        switch (choice) {
+
+            case 1:
+
+                EnterA();
+
+                break;
+
+            case 2:
+
+                EnterB();
+
+                break;
+
+            case 3:
+
+                FindS();
+
+                break;
+
+            case 4:
+
+                FindP();
+
+                break;
+
+            case 5:
+
+                exitProgram = true;
+
+                cout << "Программа завершена." << endl;
+
+                break;
+
+            default:
+
+                cout << "Ошибка. Неверный выбор! Попробуйте снова." << endl;
+
+                break;
+
+        }
+
+    }
 }
